c07128a00dbdeef6f274d659470f22755075059b
Bastian Meier <post@bastianmeier.de>
2013-03-21 23:41:05 +0100

    updating demo

--- demo/ajax.php
+++ demo/ajax.php
@@ -2,68 +2,64 @@
 
 
 $json2html= 
-	'
-	"json2html":
-		[{	
-			"classname": "special_class",
+'
+"json2html":
+	[{	
+		"classname": "special_class",
+		"tag": "div",
+		"content": 
+			[{
 			"tag": "div",
+			"id": "div_id",
+			"content": "test"	
+			},
+			{
+			"tag": "p",
+			"id": "p_id_2",
 			"content": 
 				[{
-				"tag": "div",
-				"id": "p_id",
-				"content": "test"	
-				},
-				{
-				"tag": "p",
-				"id": "p_id_2",
-				"content": 
-					[{
-						"content": "test2"
-					},{
-						"tag": "span",
-						"classname": "span_class",
-						"content": " test3",
-					},{
-						"content": "test4"
-					}]	
-				},
-				{
-				"tag": "h2",
-				"content": "Überschrift"	
-				}]
-		},
-		{
+					"content": "content "
+				},{
+					"tag": "span",
+					"classname": "span_class",
+					"content": " span content "
+				},{
+					"content": " content after span"
+				}]	
+			},
+			{
 			"tag": "h2",
-			"content": "Überschrift2"	
-		},
-		{
-			"tag": "img",
-			"id": "bild",
-			"attributes": [{
-				"src": "http://stupo.ned/img/stupo_logo.png"
-			}]	
-		},
-		{
-			"tag": "link",
-			"attributes": 
-				[{
-					"rel": "stylesheet",
-					"type": "text/css",
-					"href": "http://stupo.ned/core/css/pages/start.css"
-				}]
-		},
-		{
-			"tag": "script",
-			"attributes": 
-				[{
-					"type": "text/javascript",
-					"src": "http://stupo.ned/core/js/pages/welcome.js"
-				}]
-		}]
-	
-	'
-	;
-	
+			"content": "Headline"	
+			}]
+	},
+	{
+		"tag": "br"	
+	},
+	{
+		"tag": "img",
+		"id": "nice_picture",
+		"attributes": [{
+			"src": "http://example.com/logo.png"
+		}]	
+	},
+	{
+		"tag": "link",
+		"attributes": 
+			[{
+				"rel": "stylesheet",
+				"type": "text/css",
+				"href": "http://example.com/welcome.css"
+			}]
+	},
+	{
+		"tag": "script",
+		"attributes": 
+			[{
+				"type": "text/javascript",
+				"src": "http://example.com/welcome.js"
+			}]
+	}]';
+
 	
 echo $_GET['callback'] .
 '({"result":"ok", ', stripslashes ( $json2html ),
